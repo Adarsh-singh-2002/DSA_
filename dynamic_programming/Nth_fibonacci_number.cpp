69bc84813f6c7dcba3fb0e1470d6b139b4d0c187
Adarsh <adarshriit2017@gmail.com>
2023-07-20 17:04:52 +0530

    20072023-1

---
+++ dynamic_programming/Nth_fibonacci_number.cpp
@@ -0,0 +1,26 @@
+class Solution {
+  public:
+    int nthFibonacci(int n){
+        // code here
+        int  prev2 = 1; 
+        int  prev1 = 1;
+        int  curi = 0;
+         
+        if( n == 1 || n == 2 )
+        return 1;
+        
+        for(int i = 2 ; i < n ;i++)
+        {
+            curi = (prev1+prev2)%1000000007;
+            
+            prev2 = prev1;
+            prev1 = curi;
+            
+        }
+        
+        
+        return curi;
+
+        
+    }
+};
