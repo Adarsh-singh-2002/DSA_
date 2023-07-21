441f9a6ffb139728d5e05cec6c920f0728fac1ca
Adarsh <adarshriit2017@gmail.com>
2023-07-21 23:50:52 +0530

    21072023-1

---
+++ dynamic_programming/coin_change.cpp
@@ -0,0 +1,22 @@
+long long int count(int coins[], int N, int sum) {
+
+        // code here.
+        vector<vector<long long>> dp(N,vector<long long>(sum+1,0));
+        for(int i = 0 ; i <= sum ; i++)
+        {
+            dp[0][i] = (i%coins[0]==0);
+        }
+        for(int index = 1 ; index < N ; index++)
+        {
+            for(int i = 0 ; i <= sum ;i++)
+            {
+                long long notTake = dp[index-1][i];
+                long long take = 0 ;
+                
+                if(coins[index]<=i) take = dp[index][i-coins[index]];
+                dp[index][i]=take+notTake;
+            }
+        }
+        
+        return dp[N-1][sum];
+    }
