8e1e8e903744ff5f2a49c0f2866d67a5985b6c1a
Adarsh <adarshriit2017@gmail.com>
2023-08-18 10:21:56 +0530

    18082023-1

--- dynamic_programming/longest_increasing_subsequence.cpp
+++ dynamic_programming/longest_increasing_subsequence.cpp
@@ -67,8 +67,32 @@ class Solution
     {
        // your code here
        
-       vector<vector<int>> dp(n,vector<int>(n+1,-1));
-       return getAns(a,n,0 ,-1, dp);
+    //   vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+    //   return getAns(a,n,0 ,-1, dp);
+       int dp[n+1];
+       for(int i = 1 ; i <=n ;i++)
+       {
+           dp[i] = INT_MAX;
+       }
+       dp[0] = INT_MIN;
+       
+       for(int i = 0 ; i < n ; i++)
+       {
+           int idx = upper_bound(dp,dp+n+1,a[i])-dp;
+           if(a[i]>dp[idx-1] and a[i] < dp[idx]){
+               dp[idx] = a[i];
+           }
+       }
+       int ma = 0 ; 
+       for(int i = n ; i >= 0 ; i--)
+       {
+           if(dp[i]!= INT_MAX){
+               ma = i;
+               break;
+           }
+       }
+       return ma;
+      
        
     }
 };
